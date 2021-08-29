3aa0c7a9db67a1c94031d92316ada687eec95a39
aussie <saltydoginc@gmail.com>
2021-08-30 00:43:31 +1000

    Add files via upload
    Thanks to Valken for finding these scripts on Ravage discord.

---
+++ GF-Auto-Random-Loadout-Script-version-1.1/GF_Auto_Random_Loadout-script-v1.1/+GF_Auto_Random_Loadout_Script.VR/Mod_Version/@GF_Auto_Random_Loadout/mod.cpp
@@ -0,0 +1,41 @@
+
+
+
+//________________  Author : GEORGE FLOROS [GR] ___________ 06.01.19 _____________
+
+/*
+________________ GF Auto Random Loadout Script ________________
+
+https://forums.bohemia.net/forums/topic/220873-gf-auto-random-loadout-script/
+
+Please keep the Credits or add them to your Diary
+
+https://community.bistudio.com/wiki/SQF_syntax
+Don't try to open this with the simple notepad.
+For everything that is with comment  //  in front  or between /*
+means that it is disabled , so there is no need to delete the extra lines.
+
+You can open this ex:
+with notepad++
+https://notepad-plus-plus.org/
+
+and also use the extra pluggins
+(this way will be better , it will give also some certain colors to be able to detect ex. problems )
+http://www.armaholic.com/page.php?id=8680
+
+or use any other program for editing .
+
+For the Compilation List of my GF Scripts , you can search in:
+https://forums.bohemia.net/forums/topic/215850-compilation-list-of-my-gf-scripts/
+*/
+
+
+author = "GEORGE FLOROS [GR]";
+name = "GF Auto Random Loadout";
+action = "https://forums.bohemia.net/profile/1141978-george-floros-gr/";
+actionName = "BIS Forums";
+overview = "GF Auto Random Loadout<br />Description:<br />It will detect the enabled Mods , without editing lists , posible to unpack and edit<br />by GEORGE FLOROS [GR], enjoy!";
+picture = "GF_LOGO.paa";
+logo = "GF_LOGO.paa";
+logoOver = "GF_LOGO.paa";
+logoSmall = "GF_LOGO.paa";
